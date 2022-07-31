31c389832f73119948f16d1882064279f2b26a0f
RiyaadHossain <riyadhossain.dev@gamil.com>
2022-07-31 18:31:39 +0600

    App is working

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 
 function App() {
   const counter = useSelector((state) => state.counter);
+  const dispatch = useDispatch();
+  const increment = () => {
+    dispatch({ type: "INC" });
+  };
+  const decrement = () => {
+    dispatch({ type: "DEC" });
+  };
   return (
     <div className="bg-sky-300 pt-20 text-center h-[100vh]">
       <h1 className="text-4xl font-bold text-center">Counter App</h1>
@@ -9,8 +16,18 @@ function App() {
         {counter}
       </h2>
       <div>
-        <button className="btn font-bold text-3xl w-14 h-10 m-5 rounded-lg bg-green-500">+</button>
-        <button className="btn font-bold text-3xl w-14 h-10 m-5 rounded-lg bg-red-500">-</button>
+        <button
+          onClick={increment}
+          className="btn font-bold text-3xl w-14 h-10 m-5 rounded-lg bg-green-500"
+        >
+          +
+        </button>
+        <button
+          onClick={decrement}
+          className="btn font-bold text-3xl w-14 h-10 m-5 rounded-lg bg-red-500"
+        >
+          -
+        </button>
       </div>
     </div>
   );
--- src/store/index.js
+++ src/store/index.js
@@ -2,6 +2,8 @@ import { createStore } from "redux";
 
 // Reducer Function
 const reducerFn = (state = { counter: 0 }, actions) => {
+    if (actions.type === 'INC') return {counter : state.counter + 1} // To Increment
+    if (actions.type === 'DEC') return {counter : state.counter - 1} // To Decrement
     return state
 };
 
